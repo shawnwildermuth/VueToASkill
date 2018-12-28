157f52add6b703d5765298e620285828d3862688
Shawn Wildermuth <shawn@wildermuth.com>
2018-12-28 16:02:38 -0500

    Initial Drop of the before and after

---
+++ after/wwwroot/js/contact.js
@@ -0,0 +1,19 @@
+﻿// contact.js
+Vue.use(VeeValidate);
+
+var theView = new Vue({
+  el: "#theForm",
+  data: {
+    title: "Contact Us",
+    msg: {}
+  },
+  methods: {
+    onSave: function () {
+      this.$validator.validateAll().then(function (success) {
+        if (success) {
+          alert("Trying to save: " + this.name);
+        }
+      }.bind(this));
+    }
+  }
+});
---
+++ before/Pages/Contact.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ABetterJQuery.Pages
+{
+  public class ContactModel : PageModel
+  {
+    private readonly ILogger<ContactModel> _logger;
+
+    public ContactModel(ILogger<ContactModel> logger)
+    {
+      _logger = logger;
+    }
+    public void OnPost()
+    {
+      _logger.LogInformation($"Sending Contact Page Message");
+    }
+  }
+}
